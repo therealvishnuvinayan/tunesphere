f57324ba87587ca71a8e8e5010799558fb796455
Vishnu Vinayan <therealvishnuvinayan@gmail.com>
2024-04-29 22:37:00 +0400

    dispatching the active song

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -6,10 +6,16 @@ import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
   console.log("song", song);
+  const dispatch = useDispatch();
 
-  const handlePauseClick = () => {};
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
 
-  const handlePlayClick = () => {};
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
